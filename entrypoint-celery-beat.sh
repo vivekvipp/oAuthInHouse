794aaba6a6d27cab15ea7a74abd9286a2bfab2a9
balpreetbanga18 <99896974+balpreetbanga18@users.noreply.github.com>
2024-07-03 15:05:09 +0530

    Create entrypoint-celery-beat.sh

---
+++ entrypoint-celery-beat.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+# Wait for the PostgreSQL database to be ready
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the server
+celery -A gamp_gateway beat --loglevel=info
