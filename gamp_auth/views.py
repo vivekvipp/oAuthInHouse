d734770229998fbaaa6b71df40607338e57eb362
Praveen Kumar Jha <praveen@auranft.co>
2024-07-03 19:35:24 +0530

    fix: views updation done

--- gamp_auth/views.py
+++ gamp_auth/views.py
@@ -48,9 +48,9 @@ def generate_otp(request):
             return Response({'error': 'User is blocked due to multiple incorrect OTP attempts'},
                             status=status.HTTP_403_FORBIDDEN)
 
-        latest_otp = OTP.objects.filter(user=user, is_used=False)
-        if latest_otp.count() > 1:
-            latest_otp = latest_otp.order_by('-created_at').first()
+        latest_otps = OTP.objects.filter(user=user, is_used=False).order_by('-created_at')
+        if latest_otps.count() > 1:
+            latest_otp = latest_otps.order_by('-created_at').first()
             if latest_otp.is_valid():
                 return Response({'error': 'Previous OTP is still valid'}, status=status.HTTP_400_BAD_REQUEST)
 
