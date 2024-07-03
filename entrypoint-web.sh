87a36298694b9f491d96deb89c75d2b3ebda0531
balpreetbanga18 <99896974+balpreetbanga18@users.noreply.github.com>
2024-07-03 14:45:08 +0530

    Create entrypoint-web.sh

---
+++ entrypoint-web.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+# Wait for the PostgreSQL database to be ready
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+
+# Start the server
+gunicorn gamp_gateway.wsgi:application --bind 0.0.0.0:8000 --workers 3 --threads 2 --timeout 60 --log-level=info
