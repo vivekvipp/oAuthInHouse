57658b8dc98071c0b68efc390b8e17c5a68a73de
Praveen Kumar Jha <praveen@auranft.co>
2024-07-03 19:25:36 +0530

    fix: redis set converted to string and tasks

--- gamp_auth/tasks.py
+++ gamp_auth/tasks.py
@@ -1,5 +1,5 @@
 from celery import shared_task
-from .models import OTP
+from .models import OTP, User
 from .redis_connection import RedisConnection
 
 import logging
@@ -22,11 +22,20 @@ def mark_expired_otps_inactive():
             otp_code = key.decode('utf-8').split(':')[1]
             stored_user_id = r.get(key)
 
+            if isinstance(stored_user_id, bytes):
+                stored_user_id = stored_user_id.decode('utf-8')
+            elif isinstance(stored_user_id, str):
+                stored_user_id = int(stored_user_id)
+
             if not stored_user_id:
-                otp = OTP.objects.filter(otp=otp_code, is_used=False).first()
-                if otp:
-                    otp.is_used = True
-                    otp.save()
-                    r.delete(key)
+                user_obj = User.objects.filter(id=stored_user_id)
+                if user_obj.count() > 1:
+                    user_first = user_obj.first()
+                    otp = OTP.objects.filter(otp=otp_code, user=user_first, is_used=False).first()
+                    if otp:
+                        otp.is_used = True
+                        otp.save()
+                        r.delete(key)
+                logger.warn(f"User with ID {stored_user_id} not found in the database")
     except Exception as e:
         logger.error(f"Redis connection failed: {e}")
